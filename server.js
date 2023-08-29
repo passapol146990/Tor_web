0a2575ca54085a01029159cb567e54be78617bfe
Passapol <110749961+passapol146990@users.noreply.github.com>
2023-08-29 13:34:30 +0700

    image1

--- server.js
+++ server.js
@@ -58,28 +58,28 @@ const db = mysql.createConnection ({
 
 let connection = db;
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to database:', err);
-  } else {
-    console.log('Connected to database');
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to database:', err);
+//   } else {
+//     console.log('Connected to database');
 
-    // ดึงข้อมูลตารางทั้งหมด
-    connection.query('SHOW TABLES', (err, results) => {
-      if (err) {
-        console.error('Error fetching tables:', err);
-      } else {
-        console.log('Tables in the database:');
-        results.forEach(table => {
-          console.log(table[`Tables_in_${connection.config.database}`]);
-        });
+//     // ดึงข้อมูลตารางทั้งหมด
+//     connection.query('SHOW TABLES', (err, results) => {
+//       if (err) {
+//         console.error('Error fetching tables:', err);
+//       } else {
+//         console.log('Tables in the database:');
+//         results.forEach(table => {
+//           console.log(table[`Tables_in_${connection.config.database}`]);
+//         });
 
-        // ปิดการเชื่อมต่อกับฐานข้อมูล
-        connection.end();
-      }
-    });
-  }
-});
+//         // ปิดการเชื่อมต่อกับฐานข้อมูล
+//         connection.end();
+//       }
+//     });
+//   }
+// });
 
 
 
